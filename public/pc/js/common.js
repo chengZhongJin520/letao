08c5796324510e1cd1e4948137fe9fc6cd7774fb
chengzhongji <1286178132@qq.com>
2018-03-04 20:07:56 +0800

    index 和 user 页面功能完成

--- public/pc/js/common.js
+++ public/pc/js/common.js
@@ -1,3 +1,70 @@
 /**
  * Created by Lenovo on 2018/3/2.
  */
+
+$(function(){
+
+  //进度条
+  //ajax开始
+  $(document).ajaxStart(function () {
+    NProgress.start();
+  });
+  //所有ajax结束
+  $(document).ajaxStop(function () {
+   setTimeout(function(){
+     NProgress.done();
+   },1000)
+  });
+
+
+  //每进入一个页面就判断下是否登录
+
+  if(location.href.indexOf('login.html')== -1){
+    $.ajax({
+      url:'/employee/checkRootLogin',
+      type:'get',
+      data:'',
+      success:function(info){
+        if(info.error==400){
+          location.href='login.html';
+        }
+      }
+    })
+  }
+
+
+//二级菜单点击事件
+  $('.second').prev().on('click',function(){
+      $(this).next().slideToggle(500);
+  })
+
+
+  //关闭侧边栏事件
+  $('.icon_menu').on('click',function(){
+    $('.lt_aside').toggleClass('now');
+    $('.lt_main').toggleClass('now');
+  })
+
+
+  //显示模态框
+  $(".icon_logout").on("click", function () {
+
+    $("#logoutModal").modal("show");
+
+  });
+
+
+  //退出按钮
+  $('.btn_logout').on('click',function(){
+    $.ajax({
+      type:'get',
+      url:'/employee/employeeLogout',
+      success:function(info){
+        console.log(info);
+        if(info.success){
+          location.href='login.html';
+        }
+      }
+    })
+  })
+})
--- public/pc/js/login.js
+++ public/pc/js/login.js
@@ -1,3 +1,98 @@
 /**
  * Created by Lenovo on 2018/3/2.
  */
+
+
+$(function () {
+
+  //校验表单
+  $('#form').bootstrapValidator({
+
+
+    //3. 指定校验字段
+    fields: {
+      //校验用户名，对应name表单的name属性
+      username: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '用户名不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 2,
+            max: 6,
+            message: '用户名长度必须在2到6之间'
+          },
+          callback:{
+            message:'用户名错误'
+          }
+        }
+      },
+
+
+
+      //校验密码，对应name表单的name属性
+      password: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '密码不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 6,
+            max: 10,
+            message: '用户名长度必须在6到10之间'
+          },
+          callback:{
+            message:'密码错误'
+          }
+        }
+      },
+    },
+
+
+    //配置小图标, 成功 失败  校验中
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+  })
+
+  //给登录一个事件，成功时阻止默认提交，使用ajax
+  $('#form').on('success.form.bv',function(e){
+    e.preventDefault();
+
+    //发送ajax
+    $.ajax({
+      url:'/employee/employeeLogin',
+      type:'post',
+      data: $("form").serialize(),
+      dataType:'json',
+      success:function(info){
+        console.log(info);
+        if(info.error===1000){
+           $("#form").data('bootstrapValidator').updateStatus("username", "INVALID", "callback");
+        }
+        if(info.error===1001){
+          $("#form").data('bootstrapValidator').updateStatus("password", "INVALID", "callback");
+        }
+
+        if(info.success) {
+          $("form").data("bootstrapValidator").resetForm(true);
+          location.href = "index.html";
+        }
+      }
+
+    })
+
+  })
+
+
+  //重置表单
+  $("[type='reset]").on('click',function(){
+    $("form").data("bootstrapValidator").resetForm(true);
+  })
+})
