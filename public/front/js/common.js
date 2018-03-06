f79fca940a071fc93b00bb27d26e3ada1a6f6601
chengzhongji <1286178132@qq.com>
2018-03-06 14:48:16 +0800

    移动端首页完成

---
+++ public/front/js/common.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Lenovo on 2018/3/6.
+ */
+mui(".mui-slider").slider({
+  interval:2000
+});
+mui(".mui-scroll-wrapper").scroll({
+  scrollY:true,
+  bounce:true,
+  indicators: false
+})
---
+++ public/front/js/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Lenovo on 2018/3/6.
+ */
+;(function(){
+
+})();
